81278b8637bc2f51ebc86e22546ea10c7c0cdd9f
Dave G.J <65545355+DaveGomesh@users.noreply.github.com>
2021-01-10 11:24:28 -0300

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,40 @@
+const _0x3a6e=['\x33\x39\x37\x34\x37\x5a\x4a\x44\x62\x78\x46','\x39\x34\x31\x34\x32\x6b\x54\x53\x42\x4b\x74','\x31\x4f\x75\x42\x77\x48\x51','\x41\x6c\x75','\x69\x6e\x6e','\x20\u00e9\x20','\x61\x29\x3a','\x31\x34\x33\x38\x38\x31\x64\x43\x6b\x77\x4c\x41','\x31\x66\x6d\x6f\x42\x72\x61','\x31\x38\x32\x34\x39\x37\x6e\x46\x75\x58\x51\x77','\x33\x30\x35\x31\x33\x39\x42\x49\x47\x4f\x6e\x42','\x0a\x4e\x6f','\x37\x79\x78\x70\x75\x4e\x51','\x62\x6f\x64','\x6e\x6f\x28','\x44\x61\x76','\x6e\x6f\x74','\x32\x4a\x4a\x76\x64\x41\x58','\x74\x61\x3a','\x31\x34\x38\x35\x36\x37\x4b\x75\x74\x52\x74\x42','\x36\x34\x38\x36\x37\x41\x4c\x78\x4b\x4f\x65'];const _0x1fd0=function(_0x3c4b8d,_0x5ab1b6){_0x3c4b8d=_0x3c4b8d-(0xcad+-0x1*-0x8cd+0x157a*-0x1);let _0x32c70b=_0x3a6e[_0x3c4b8d];return _0x32c70b;};const _0x972ec6=_0x1fd0;(function(_0x22b957,_0x11d5cb){const _0x4387e0=_0x1fd0;while(!![]){try{const _0x47a4eb=-parseInt(_0x4387e0(0x0))*-parseInt(_0x4387e0(0x10))+-parseInt(_0x4387e0(0x14))*-parseInt(_0x4387e0(0xb))+parseInt(_0x4387e0(0x8))+-parseInt(_0x4387e0(0x1))*parseInt(_0x4387e0(0x6))+-parseInt(_0x4387e0(0x9))+parseInt(_0x4387e0(0x12))+parseInt(_0x4387e0(0x7))*-parseInt(_0x4387e0(0x13));if(_0x47a4eb===_0x11d5cb)break;else _0x22b957['push'](_0x22b957['shift']());}catch(_0x2eb5d4){_0x22b957['push'](_0x22b957['shift']());}}}(_0x3a6e,-0x879ed+-0x59651*0x1+0x24c8d*0x8));let _0x5752ee=_0x972ec6(0xe)+'\x69\x64',_0x1287e3=Number(0x30e*0x9+0x173*-0x1+0x19f7*-0x1-(-0x1782*-0x1+0x4a1+-0x1c19));document[_0x972ec6(0xc)+'\x79'][_0x972ec6(0x3)+'\x65\x72\x48'+'\x54\x4d\x4c']=_0x5752ee+(_0x972ec6(0x4)+_0x972ec6(0xf)+'\x61\x20')+_0x1287e3,console['\x6c\x6f\x67'](_0x972ec6(0x2)+_0x972ec6(0xd)+_0x972ec6(0x5)+'\x20'+_0x5752ee+(_0x972ec6(0xa)+_0x972ec6(0x11)+'\x20')+_0x1287e3);
+
+// const _0x5a62=['\x62\x6f\x64','\x31\x6a\x5a\x50\x76\x72\x76','\x33\x36\x30\x34\x38\x36\x6a\x78\x4f\x73\x71\x75','\x6e\x6f\x28','\x38\x39\x36\x30\x33\x4c\x49\x45\x61\x6a\x46','\x41\x6c\x75','\x69\x6e\x6e','\x20\u00e9\x20','\x36\x38\x6e\x47\x73\x51\x55\x7a','\x6c\x6f\x67','\x0a\x4e\x6f','\x65\x72\x48','\x31\x36\x37\x33\x33\x30\x4d\x74\x6b\x4c\x78\x6f','\x34\x62\x6d\x51\x42\x46\x6f','\x74\x61\x3a','\x61\x29\x3a','\x31\x34\x31\x31\x36\x37\x48\x44\x67\x70\x44\x69','\x37\x30\x31\x30\x4b\x73\x75\x67\x78\x78','\x33\x37\x36\x39\x30\x31\x55\x57\x65\x57\x6a\x75','\x31\x30\x30\x39\x39\x33\x67\x6e\x6a\x63\x61\x52','\x6e\x6f\x74','\x4c\x75\x63'];const _0x4d66=function(_0x117bbf,_0x48debb){_0x117bbf=_0x117bbf-(0xfff*0x1+-0x117e*-0x2+0x1f*-0x1a5);let _0x2fe3e2=_0x5a62[_0x117bbf];return _0x2fe3e2;};const _0x1cc3ee=_0x4d66;(function(_0x5493c6,_0x308716){const _0x1cc9ca=_0x4d66;while(!![]){try{const _0x2dfd92=parseInt(_0x1cc9ca(0xa))*-parseInt(_0x1cc9ca(0x1))+parseInt(_0x1cc9ca(0x6))*parseInt(_0x1cc9ca(0xc))+-parseInt(_0x1cc9ca(0x5))+parseInt(_0x1cc9ca(0x11))+parseInt(_0x1cc9ca(0xb))+-parseInt(_0x1cc9ca(0x9))*parseInt(_0x1cc9ca(0x10))+-parseInt(_0x1cc9ca(0x13));if(_0x2dfd92===_0x308716)break;else _0x5493c6['push'](_0x5493c6['shift']());}catch(_0x44dee0){_0x5493c6['push'](_0x5493c6['shift']());}}}(_0x5a62,-0x7ff2*0x2+0x15efb+0x3b23c));let _0x1259cb=_0x1cc3ee(0xe)+'\x61\x73',_0x235414=Number(0x26cb+0x2*0x25f+-0x2b75-(0x8b*0x26+0x6*0x7+0x2*-0xa61));document[_0x1cc3ee(0xf)+'\x79'][_0x1cc3ee(0x15)+_0x1cc3ee(0x4)+'\x54\x4d\x4c']=_0x1259cb+(_0x1cc3ee(0x0)+_0x1cc3ee(0xd)+'\x61\x20')+_0x235414,console[_0x1cc3ee(0x2)](_0x1cc3ee(0x14)+_0x1cc3ee(0x12)+_0x1cc3ee(0x8)+'\x20'+_0x1259cb+(_0x1cc3ee(0x3)+_0x1cc3ee(0x7)+'\x20')+_0x235414);
+
+// let _0x4436e2='\x4c\x75\x63'+'\x61\x73',_0x1259cb=Number(0xfff*0x1+-0x117e*-0x2+0x53*-0x9d-(0x4f3*-0x5+0x13*0x116+0x1*0x427));document['\x62\x6f\x64'+'\x79']['\x69\x6e\x6e'+'\x65\x72\x48'+'\x54\x4d\x4c']=_0x4436e2+('\x20\u00e9\x20'+'\x6e\x6f\x74'+'\x61\x20')+_0x1259cb,console['\x6c\x6f\x67']('\x41\x6c\x75'+'\x6e\x6f\x28'+'\x61\x29\x3a'+'\x20'+_0x4436e2+('\x0a\x4e\x6f'+'\x74\x61\x3a'+'\x20')+_0x1259cb);
+
+// let _0x4436e2 = '\x4c\x75\x63' + '\x61\x73', _0x1259cb = Number(0xfff * 0x1 + -0x117e * -0x2 + 0x53 * -0x9d - (0x4f3 * -0x5 + 0x13 * 0x116 + 0x1 * 0x427));
+// document['\x62\x6f\x64' + '\x79']['\x69\x6e\x6e' + '\x65\x72\x48' + '\x54\x4d\x4c'] = _0x4436e2 + ('\x20\u00e9\x20' + '\x6e\x6f\x74' + '\x61\x20') + _0x1259cb, console['\x6c\x6f\x67']('\x41\x6c\x75' + '\x6e\x6f\x28' + '\x61\x29\x3a' + '\x20' + _0x4436e2 + ('\x0a\x4e\x6f' + '\x74\x61\x3a' + '\x20') + _0x1259cb);
+
+// let _0x306dcb = '\x4c\x75\x63' + '\x61\x73', _0xc0e5f = Number(0x5da + -0x10 * 0x198 + 0x13ba - (-0x2264 + -0x5a2 + 0x2810 * 0x1));
+// document['\x62\x6f\x64' + '\x79']['\x69\x6e\x6e' + '\x65\x72\x48' + '\x54\x4d\x4c'] = _0x306dcb + ('\x20\u00e9\x20' + '\x6e\x6f\x74' + '\x61\x20') + _0xc0e5f, console['\x6c\x6f\x67']('\x41\x6c\x75' + '\x6e\x6f\x28' + '\x61\x29\x3a' + '\x20' + _0x306dcb + ('\x0a\x4e\x6f' + '\x74\x61\x3a' + '\x20') + _0xc0e5f);
+
+// let _0x17a864 = 'Luc' + 'as', _0x751482 = Number(-0xac8 + 0x1 * -0x1ba6 + 0x2682 - (0x11c7 + -0x1cba + 0xafd));
+// document['bod' + 'y']['inn' + 'erH' + 'TML'] = _0x17a864 + ('\x20é\x20' + 'not' + 'a\x20') + _0x751482, console['log']('Alu' + 'no(' + 'a):' + '\x20' + _0x17a864 + ('\x0aNo' + 'ta:' + '\x20') + _0x751482);
+
+// let _0x42ce6e = 'Luc' + 'as', _0x4ef67f = Number(0x14 - 0xa);
+// document['bod' + 'y']['inn' + 'erH' + 'TML'] = _0x42ce6e + ('\x20é\x20' + 'not' + 'a\x20') + _0x4ef67f, console['log']('Alu' + 'no(' + 'a):' + '\x20' + _0x42ce6e + ('\x0aNo' + 'ta:' + '\x20') + _0x4ef67f);
+
+// let _0x1197e9 = 'Lucas', _0x5e9782 = Number(0x14 - 0xa);
+// document['body']['innerHTML'] = _0x1197e9 + '\x20é\x20nota\x20' + _0x5e9782, console['log']('Aluno(a):\x20' + _0x1197e9 + '\x0aNota:\x20' + _0x5e9782);
+
+// let nome = 'Lucas', nota = Number(0x14 - 0xa);
+// document['body']['innerHTML'] = nome + '\x20é\x20nota\x20' + nota, console['log']('Aluno(a):\x20' + nome + '\x0aNota:\x20' + nota);
+
+// let nome = 'Lucas';
+// let nota = Number(0x14 - 0xa);
+// document['body']['innerHTML'] = nome + '\x20é\x20nota\x20' + nota;
+// console['log']('Aluno(a):\x20' + nome + '\x0aNota:\x20' + nota);
+
+
+
+// //variaveis
+// let nome = "Lucas";
+// let nota = Number(20 - 10);
+
+// //DOM
+// document.body.innerHTML = `${nome} é nota ${nota}`;
+
+// //Console
+// console.log(`Aluno(a): ${nome}\nNota: ${nota}`);
